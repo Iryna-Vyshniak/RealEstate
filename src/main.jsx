edbe971c31ba52f72e4ff1cac3c5920a9d8878a2
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-23 02:00:31 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
-import React, { Suspense } from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Suspense fallback={<>Loading...</>}>
-    <BrowserRouter basename="/RealEstate">
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </BrowserRouter>
-  </Suspense>
+  <BrowserRouter basename="/RealEstate">
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </BrowserRouter>
 );
