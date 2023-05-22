1c70931ae5d1048b4923fe2d832aaa74f13bcb56
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-22 23:00:39 +0300

    change title

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import './App.css';
-
 function App() {
-  return <h1 className='text-3xl font-bold underline bg-slate-500'>RealEstate</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline bg-slate-500">RealEstate</h1>
+  );
 }
 
 export default App;
