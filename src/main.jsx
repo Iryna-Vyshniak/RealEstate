a0f54d257c6458eb49e6479a4ee03d487c83b349
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-22 23:43:20 +0300

    add  Suspense and BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <Suspense fallback={<>Loading...</>}>
+    <BrowserRouter basename="/RealEstate">
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Suspense>
+);
