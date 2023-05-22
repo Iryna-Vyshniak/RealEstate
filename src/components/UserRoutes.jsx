b238afa559c986f1c45001f7ce5fccb7bca0f05e
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-23 00:19:54 +0300

    add routes

--- src/components/UserRoutes.jsx
+++ src/components/UserRoutes.jsx
@@ -3,12 +3,23 @@ import { Routes, Route } from 'react-router-dom';
 
 const Layout = lazy(() => import('./Layout/Layout'));
 const HomePage = lazy(() => import('../pages/HomePage'));
+const ProfilePage = lazy(() => import('../pages/ProfilePage'));
+const SignInPage = lazy(() => import('../pages/SignInPage'));
+const SignUpPage = lazy(() => import('../pages/SignUpPage'));
+const ForgotPasswordPage = lazy(() => import('../pages/ForgotPasswordPage'));
+const OffersPage = lazy(() => import('../pages/OffersPage'));
 
 const UserRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/offers" element={<OffersPage />} />
+        <Route path="*" element={<HomePage />} />
       </Route>
     </Routes>
   );
