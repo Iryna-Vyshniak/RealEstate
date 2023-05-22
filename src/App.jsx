d3359c19cc48a9def95ae13545ba452bae17c544
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-23 02:02:36 +0300

    add UserRoutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
+import UserRoutes from './components/UserRoutes';
+
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline bg-slate-500">RealEstate</h1>
-  );
+  return <UserRoutes />;
 }
 
 export default App;
