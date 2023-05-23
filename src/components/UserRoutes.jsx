aa5d89ef2557637a861eb6cd08554f5efe33b281
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-23 13:08:02 +0300

    refactor lazy

--- src/components/UserRoutes.jsx
+++ src/components/UserRoutes.jsx
@@ -1,23 +1,14 @@
-// import { lazy, Suspense } from 'react';
-import { Suspense } from 'react';
+import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Layout } from './Layout/Layout';
-import HomePage from '../pages/HomePage';
-import ProfilePage from '../pages/ProfilePage';
-import SignInPage from '../pages/SignInPage';
-import SignUpPage from '../pages/SignUpPage';
-import ForgotPasswordPage from '../pages/ForgotPasswordPage';
-import OffersPage from '../pages/OffersPage';
-import PageNotFound from '../pages/PageNotFound';
 
-// const Layout = lazy(() => import('./Layout/Layout'));
-// const HomePage = lazy(() => import('../pages/HomePage'));
-// const ProfilePage = lazy(() => import('../pages/ProfilePage'));
-// const SignInPage = lazy(() => import('../pages/SignInPage'));
-// const SignUpPage = lazy(() => import('../pages/SignUpPage'));
-// const ForgotPasswordPage = lazy(() => import('../pages/ForgotPasswordPage'));
-// const OffersPage = lazy(() => import('../pages/OffersPage'));
-// const PageNotFound = lazy(() => import('../pages/PageNotFound'));
+const Layout = lazy(() => import('./Layout/Layout'));
+const HomePage = lazy(() => import('../pages/HomePage'));
+const ProfilePage = lazy(() => import('../pages/ProfilePage'));
+const SignInPage = lazy(() => import('../pages/SignInPage'));
+const SignUpPage = lazy(() => import('../pages/SignUpPage'));
+const ForgotPasswordPage = lazy(() => import('../pages/ForgotPasswordPage'));
+const OffersPage = lazy(() => import('../pages/OffersPage'));
+const PageNotFound = lazy(() => import('../pages/PageNotFound'));
 
 const UserRoutes = () => {
   return (
