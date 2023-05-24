44068c515a31764c6697c49bd0236fc34fb1636a
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-24 12:39:04 +0300

    add navigation

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,15 +1,16 @@
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 export const Header = () => {
   const activeLink =
     'cursor-pointer py-3 text-sm font-semibold text-black-700 border-b-[3px] border-b-red-700';
+
   return (
     <div className="bg-white border-b shadow-sm sticky top-0 z-40">
       <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
         <div>
-          <p className="h-5 cursor-pointer hover:text-red-700">
+          <Link to="/" className="h-5 cursor-pointer hover:text-red-700">
             <span className="text-red-700">Real</span>Estate
-          </p>
+          </Link>
         </div>
         <nav>
           <ul className="flex space-x-10">
