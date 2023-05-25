82ed8a6261b4cfb00d06f005fcfbb2093644af09
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-25 16:26:18 +0300

    add ToastContainer for notifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import UserRoutes from './components/UserRoutes';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  return <UserRoutes />;
+  return (
+    <>
+      <UserRoutes />
+      <ToastContainer
+        position="bottom-center"
+        autoClose={1500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+    </>
+  );
 }
 
 export default App;
