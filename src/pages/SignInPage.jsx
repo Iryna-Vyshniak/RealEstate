7128eac240c7cba4a0ec9a3f77bc6da09b3ea15b
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-25 17:24:46 +0300

    complete the Sign in functionality

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { signInWithEmailAndPassword, getAuth } from 'firebase/auth';
+import { toast } from 'react-toastify';
 import OAuth from '../components/OAuth';
 
 const SignInPage = () => {
@@ -11,13 +13,30 @@ const SignInPage = () => {
   });
 
   const { email, password } = formData;
+  const navigate = useNavigate();
 
   function onChange(e) {
-    //console.log(e.target.value);
     setFormData(prevState => ({
       ...prevState,
       [e.target.id]: e.target.value,
     }));
+  }
+
+  async function onSubmit(e) {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      if (userCredential.user) {
+        navigate('/');
+      }
+    } catch (error) {
+      toast.error('Bad user credentials');
+    }
   }
 
   return (
@@ -32,7 +51,7 @@ const SignInPage = () => {
           />
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form>
+          <form onSubmit={onSubmit}>
             <input
               type="email"
               id="email"
