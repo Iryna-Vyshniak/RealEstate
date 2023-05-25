2e2022f70d889a5c032b05e54c990286bbc59b34
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-25 12:46:18 +0300

    add starting form for registration, add vissible/unvissible password

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,5 +1,70 @@
+import { useState } from 'react';
+import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
+
 const SignInPage = () => {
-  return <div>SignInPage</div>;
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  function onChange(e) {
+    //console.log(e.target.value);
+    setFormData(prevState => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  }
+
+  return (
+    <section>
+      <h1 className="text-3xl text-center mt-6 fot-bold">Sign In</h1>
+      <div className="flex justify-center items-center flex-wrap px-6 py-12 max-w-6xl mx-auto ">
+        <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
+          <img
+            src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1073&q=80"
+            alt="key"
+            className="w-full rounded-2xl"
+          />
+        </div>
+        <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
+          <form>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={onChange}
+              placeholder="Email address"
+              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+            />
+            <div className="relative mb-6">
+              <input
+                type={showPassword ? 'text' : 'password'}
+                id="password"
+                value={password}
+                onChange={onChange}
+                placeholder="Password"
+                className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+              />
+              {showPassword ? (
+                <AiFillEyeInvisible
+                  className="absolute right-3 top-3 text-xl cursor-pointer"
+                  onClick={() => setShowPassword(prevState => !prevState)}
+                />
+              ) : (
+                <AiFillEye
+                  className="absolute right-3 top-3 text-xl cursor-pointer"
+                  onClick={() => setShowPassword(prevState => !prevState)}
+                />
+              )}
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SignInPage;
