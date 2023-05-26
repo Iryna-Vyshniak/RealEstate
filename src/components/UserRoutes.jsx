f62a376c292b8e91dd9b26cf073026d04e724770
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-26 17:49:47 +0300

    add route for private route for protection profile page

--- src/components/UserRoutes.jsx
+++ src/components/UserRoutes.jsx
@@ -1,14 +1,15 @@
 import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import ProfilePage from '../pages/ProfilePage';
 
 const Layout = lazy(() => import('./Layout/Layout'));
 const HomePage = lazy(() => import('../pages/HomePage'));
-const ProfilePage = lazy(() => import('../pages/ProfilePage'));
 const SignInPage = lazy(() => import('../pages/SignInPage'));
 const SignUpPage = lazy(() => import('../pages/SignUpPage'));
 const ForgotPasswordPage = lazy(() => import('../pages/ForgotPasswordPage'));
 const OffersPage = lazy(() => import('../pages/OffersPage'));
 const PageNotFound = lazy(() => import('../pages/PageNotFound'));
+const PrivateRoute = lazy(() => import('../components/PrivateRoute'));
 
 const UserRoutes = () => {
   return (
@@ -16,7 +17,9 @@ const UserRoutes = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
-          <Route path="profile" element={<ProfilePage />} />
+          <Route path="profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<ProfilePage />} />
+          </Route>
           <Route path="sign-in" element={<SignInPage />} />
           <Route path="sign-up" element={<SignUpPage />} />
           <Route path="forgot-password" element={<ForgotPasswordPage />} />
