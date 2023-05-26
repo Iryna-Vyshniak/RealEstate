f2b40057be3974ac8cf0b350e279a0f5d00a7f3c
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-26 17:47:24 +0300

    create custom hook for protection profile page

---
+++ src/hooks/useAuthStatus.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
+export function useAuthStatus() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [checkingStatus, setCheckingStatus] = useState(true);
+
+  useEffect(() => {
+    const auth = getAuth();
+
+    onAuthStateChanged(auth, user => {
+      if (user) {
+        setLoggedIn(true);
+      }
+      setCheckingStatus(false);
+    });
+  }, []);
+
+  return { loggedIn, checkingStatus };
+}
