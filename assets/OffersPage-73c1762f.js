72e3a462b5e0a84ff8a21e3a9f96521bb30df81a
Iryna-Vyshniak <Iryna-Vyshniak@users.noreply.github.com>
2023-06-15 16:38:06 +0000

    deploy: db12eaa2825794401757e0b15a75f0bb2396a96d

--- assets/OffersPage-73c1762f.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-90382c5c.js";const s=()=>r.jsx("div",{children:"Offers"});export{s as default};
---
+++ assets/OffersPage-cc2a90e5.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-b5b08a3f.js";const s=()=>r.jsx("div",{children:"Offers"});export{s as default};
