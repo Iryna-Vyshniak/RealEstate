cf080af38ca6125ec5fc5847f7806e957c2e753c
Iryna-Vyshniak <Iryna-Vyshniak@users.noreply.github.com>
2023-06-15 20:03:36 +0000

    deploy: ccc9eb39c6836ed9fa91f952d57498e511b6b578

---
+++ assets/HomePage-06766ebf.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-041b8032.js";const r=()=>e.jsx("div",{children:"HomePage"});export{r as default};
--- assets/OffersPage-e17d8aa3.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-e20824ce.js";const s=()=>r.jsx("div",{children:"Offers"});export{s as default};
